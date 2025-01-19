72b8f6bcd1ebcb5e2eed132a87830502149290d8
purplepanda890752 <temu.lover230@gmail.com>
2025-01-19 20:19:10 +0300

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+print("Welcome to the world of Programming")
+
+print(4)
+
+print("Hello Python \n")
+
+print("hello", 6)
+
+print("Welcome to", ends="*")
---
+++ variables.py
@@ -0,0 +1,8 @@
+x = 5
+y="Billy Bob"
+print(x)
+print(y)
+
+coding="Coding"
+Name=input("enter your name")
+print("\n Hello", Name,"\n Welcome to Coding")
