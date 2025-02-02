84576293182e4190a8db3e1a79eaee2020c42cb2
purplepanda890752 <temu.lover230@gmail.com>
2025-02-02 20:30:31 +0300

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,9 @@
+string = input("Please enter any word")
+
+string2 = ('')
+
+for i in string:
+    strin2 = i + string2
+
+print("\n Original string was ", string)
+print("\n Reversed string is ", string2)
