e1898966077186b5044adc03c7a3fcaafc271ff9
purplepanda890752 <temu.lover230@gmail.com>
2025-02-08 18:32:35 +0300

    Add files via upload

---
+++ star.py
@@ -0,0 +1,22 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+Polygon = turtle .Turtle()
+ 
+ Polygon.forward(100)
+Polygon.left(120)
+Polygon.forward(100)
+Polygon.left(100)
+Polygon.forward(100)
+Polygon.penup()
+Polygon.right(100)
+Polygon.forward(100)
+
+Polygon.pendown()
+Polygon.right(100)
+Polygon.forward(100)
+Polygon.right(100)
+Polygon.forward(100)
+Polygon.right(100)
+Polygon.forward(100)
+
+turtle.done()
