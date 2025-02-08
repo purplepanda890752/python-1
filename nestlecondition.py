141fd81962dc3473cffc3aef462b3886fe3e5b3a
purplepanda890752 <temu.lover230@gmail.com>
2025-02-08 18:17:57 +0300

    Add files via upload

---
+++ nestlecondition.py
@@ -0,0 +1,14 @@
+number= int(input(enter a number))
+
+if number>1:
+    for 1 in range(2, int(number**o.5))+1:
+    if number % i==0
+    print("it is not a prime number")
+    break
+else:
+
+print ("it is not a prime number")
+
+else:
+
+print ("it is not a prime number")
