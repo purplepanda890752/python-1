4553efc5e05cbba38bb1f030c73b748da3b69c7c
purplepanda890752 <temu.lover230@gmail.com>
2025-02-23 20:22:15 +0300

    Add files via upload

--- list.py
+++ list.py
@@ -4,9 +4,9 @@ print()
 num = [1,2,3,4,5,6,7]
 print(num)
 
-tripples = [1,2,3] * 3
+tripples=[1,2,4]*4
 print(tripples)
 
 list1 = [200,300,500,600,700,800]
-revlist1 = list1[ : :-1] 
-print(revlist1, "\n")
+revlist1 = list1[: : -1] #name [starting value : ending value : steps]
+print(revlist1, "\n")    #if we dont give the starting and ending value it wi
