7d6fade2cfd89e42e3f978d0437525fe22387f7e
purplepanda890752 <temu.lover230@gmail.com>
2025-02-23 20:16:21 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+empty_list = []
+print()
+
+num = [1,2,3,4,5,6,7]
+print(num)
+
+tripples = [1,2,3] * 3
+print(tripples)
+
+list1 = [200,300,500,600,700,800]
+revlist1 = list1[ : :-1] 
+print(revlist1, "\n")
