807ab8fecfa0a8ae0bd158e1b73f3362a7dc1933
purplepanda890752 <temu.lover230@gmail.com>
2025-03-02 18:38:23 +0300

    Add files via upload

---
+++ country code.py
@@ -0,0 +1,8 @@
+country_code = {'Kenya': '254',
+                'India': '91',
+                'Saudi': '966'}
+print("Country code for India is ")
+print(country_code.get('India', 'Not Found'))
+
+print("Counry code for Japan")
+print(country_code.get('Japan', 'Not Found'))
