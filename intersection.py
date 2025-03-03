82fd9db6ebc348f27c3021b4af0defc5bab4f7b0
purplepanda890752 <temu.lover230@gmail.com>
2025-03-03 16:48:17 +0300

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,8 @@
+set1 = {"green", "red"}
+set2 = {"violet", "yellow"}
+print("original set")
+print(set1)
+print(set2)
+print("\n Intersection of two sets are: ")
+set3 = set1.intersection(set2)
+print(set3)
