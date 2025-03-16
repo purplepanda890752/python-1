2016c6cf9c1fef385c3d132330ed7b755da3ccf4
purplepanda890752 <temu.lover230@gmail.com>
2025-03-16 18:10:52 +0300

    Add files via upload

--- inheritance.py
+++ inheritance.py
@@ -1,11 +1,12 @@
-class vheicle:
+class Vehicle:
 
-    def _init_(self,name,max_speed, mileage):
-        self.name=name
-        self.max_speed= max_speed
-        self.mileage= mileage
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
 
-class Bus(vheicle):
-            pass
-school_bus = Bus("School Volvo",180 12)
-print("Vheicle name:", School_bus.name, "speed:", school_bus.max_speed, "Mileage:", School_bus.mileage)
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
