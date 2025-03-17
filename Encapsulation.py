105cb0dc7f715d0d48ee5359eeca5f8e48471038
purplepanda890752 <temu.lover230@gmail.com>
2025-03-17 16:19:13 +0300

    Add files via upload

---
+++ Encapsulation.py
@@ -0,0 +1,12 @@
+class myClass:
+    #private variable
+    _privateVar = 27
+    def_privMeth(self)
+    print("I'm inside class myclass")
+
+    def hello(self):
+        print("private variable value: ",myclass._privateVar)
+
+foo= my class()
+    foo.hello
+    foo._privMeth
