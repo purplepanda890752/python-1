1b4983d9fb836ce39fca23c98d761c249bd91594
purplepanda890752 <temu.lover230@gmail.com>
2025-03-17 16:27:42 +0300

    Add files via upload

---
+++ classcomputer.py
@@ -0,0 +1,19 @@
+class computer:
+
+    def _init_(self):
+        self._maxprice= 900
+
+        def sell(self):
+            print("selling price : ()".format(self._maxprice))
+
+            def  setmaxprice(self,price):
+                self._maxprice = price
+
+                C=computer()
+                C.sell()
+
+                c._maxprice=100
+                C.sell()
+
+c._maxprice=100
+C.sell()
