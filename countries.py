4325d730d8fe56b96dc8b0b7519c79f3d070f00c
purplepanda890752 <temu.lover230@gmail.com>
2025-03-22 18:40:11 +0300

    Add files via upload

---
+++ countries.py
@@ -0,0 +1,19 @@
+class Jamaica():
+    def capital(self):
+        print("Kingston is the capital of Jamaica")
+
+    def language(self):
+        print("English is the most spoken lanuage in Jamaica")
+
+    def type(self):
+        ("Jamaica is known as the Land of Wood and Water")
+
+class Egypt():
+    def capital(self):
+        print("Cairo is the capital of Egypt")
+    
+    def language(self):
+        print("Arabic is the most widely spoken language in Egypt")
+
+    def type(self):
+        print("Egypt is known for the world's largest collection of ancient Egyptian artifacts")
