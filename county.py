90d874a6e08db888c97901c0a749ebd8b5f5f651
purplepanda890752 <temu.lover230@gmail.com>
2025-03-22 18:38:43 +0300

    Add files via upload

---
+++ county.py
@@ -0,0 +1,13 @@
+class USA:
+    print ("washington DC is the capital of America.")
+    def english(self):
+        print(English and spanish are the most widely spoken language in america.)
+        def type (self):
+        print("USA is a developed country")
+        
+        #class 2
+        print ("Mogadishu is the capital of somalia.")
+    def somalia(self):
+     print(somalia is the most widely spoken language in somlaia.)
+    def type (self):
+        print("Somalia is a developing country")
